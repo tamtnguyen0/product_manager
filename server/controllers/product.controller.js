eee2519f1c854cf3f338466556a335d89641b251
tamtnguyen0 <59792494+tamtnguyen0@users.noreply.github.com>
2020-03-10 18:42:46 -0700

    Project Manager part 2 submission

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -16,3 +16,9 @@ module.exports.showAllProducts = (req, res) => {
         .then(products => res.json(products))
         .catch(err => res.json(err));
 }
+
+module.exports.showOneProduct = (req, res) => {
+    Product.findOne({_id: req.params.id})
+        .then(product => res.json(product))
+        .catch(err => res.json(err));
+}
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,6 +1,7 @@
 const ProductController = require('../controllers/product.controller')
 
 module.exports = app => {
-    app.get('/api', ProductController.showAllProducts);
+    app.get('/api/all', ProductController.showAllProducts);
     app.post('/api/new', ProductController.addProduct);
+    app.get('/api/product/:id', ProductController.showOneProduct);
 }
