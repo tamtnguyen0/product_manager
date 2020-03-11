2c77e9f4aeac79284586727e8ab70312218625e6
tamtnguyen0 <59792494+tamtnguyen0@users.noreply.github.com>
2020-03-10 20:07:49 -0700

    Commit for Product Manager part 3

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -22,3 +22,15 @@ module.exports.showOneProduct = (req, res) => {
         .then(product => res.json(product))
         .catch(err => res.json(err));
 }
+
+module.exports.updateProduct = (req, res) => {
+    Product.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+        .then(product => res.json(product))
+        .catch(err => res.json(err));
+}
+
+module.exports.deleteProduct = (req, res) => {
+    Product.deleteOne({_id: req.params.id})
+        .then(response => res.json(response))
+        .catch(error => res.json(error));
+}
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -4,4 +4,6 @@ module.exports = app => {
     app.get('/api/all', ProductController.showAllProducts);
     app.post('/api/new', ProductController.addProduct);
     app.get('/api/product/:id', ProductController.showOneProduct);
+    app.put('/api/product/:id', ProductController.updateProduct);
+    app.delete('/api/product/:id', ProductController.deleteProduct);
 }
